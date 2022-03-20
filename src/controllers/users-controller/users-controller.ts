b1aaae9f9de4b7c2b6d23a15b384ba95e6691c46
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-03-20 23:17:28 +0200

    chore(activities): added search activities with user id

--- src/controllers/users-controller/users-controller.ts
+++ src/controllers/users-controller/users-controller.ts
@@ -7,18 +7,18 @@ import { CustomResponse } from '../../utils/custom-response';
 import { EStatusCode } from '../../enums/EStatusCode';
 
 const {OK, BAD_REQUEST} = EStatusCode;
-export class TagsController {
+export class UsersController {
     private stackExchangeApiService: StackExchangeApiService;
     private query: string;
     constructor(@Inject stackExchangeApiService: StackExchangeApiService){
         this.stackExchangeApiService = stackExchangeApiService;
-        this.query = `${process.env.STACKEXCHANGE_API}/search/advanced?key=${process.env.STACKEXCHANGE_APPS_KEY}&site=stackoverflow&order=desc&sort=activity&filter=default`;
+        this.query = `${process.env.STACKEXCHANGE_API}/search/advanced?key=${process.env.STACKEXCHANGE_APPS_KEY}&site=stackoverflow&order=desc&sort=activity`;
     }
 
-    public async getBySingleTag(req: Request, res: Response): Promise<Response>{
-       const {tag} = req.query;
-       if(!tag) return res.status(BAD_REQUEST).send(new CustomResponse(false, {message: "tag is required"}));
-       const data = await this.stackExchangeApiService.searchByQuery(this.query+`&tagged=${tag}`);
+    public async searchByUserId(req: Request, res: Response): Promise<Response>{
+       const {id} = req.params;
+       if(!id) return res.status(BAD_REQUEST).send(new CustomResponse(false, {message: "userId is required"}));
+       const data = await this.stackExchangeApiService.searchByQuery(this.query+`&user=${id}`);
        return res.status(OK).send(new CustomResponse(true, data));
     }
 }
