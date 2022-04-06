16d1ef2ff4607d3daaab9a18e94e7d4708f5029b
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-04-06 02:10:23 +0200

    ft(auth): added github auth

---
+++ src/routes/github-auth.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { GithubStrategyType } from '../middlewares/passport/passport-strategy-type';
+import { login, logout } from '../controllers/auth-controller';
+import passport from '../strategies/passport.strategy';
+
+const router = Router();
+
+router.get('/',passport.authenticate('github'));
+
+router.get('/callback', GithubStrategyType,login);
+
+router.get('/logout', logout);
+
+export default router;
