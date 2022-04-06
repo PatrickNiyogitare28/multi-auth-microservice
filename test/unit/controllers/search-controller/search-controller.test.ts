0642e9e955a1cccd4957c81702475fea32e04da7
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-04-06 02:14:44 +0200

    bug(test): fixed tests

--- test/unit/controllers/search-controller/search-controller.test.ts
+++ test/unit/controllers/search-controller/search-controller.test.ts
@@ -1,5 +1,5 @@
 /*eslint curly: ["error", "multi"]*/
-import { APP_URL } from '../../../utils/constants';
+import App from '../../../../src/app';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
 chai.use(chaiHttp);
@@ -7,7 +7,7 @@ chai.use(chaiHttp);
 describe('GET api/v1/search/full-text-search', () => {
   it('Should return data list on successfully search by full text', (done) => {
     chai
-      .request(APP_URL)
+      .request(App)
       .get('/api/v1/search/full-text-search?text=Deploying on Azure')
       .end((err, response) => {
         if (err) return done(err);
@@ -21,7 +21,7 @@ describe('GET api/v1/search/full-text-search', () => {
 describe('GET api/v1/search/tags', () => {
   it('returns data when searching by tag', (done) => {
     chai
-      .request(APP_URL)
+      .request(App)
       .get('/api/v1/search/tags?tag=javascript')
       .end((err, response) => {
         if (err) return done(err);
@@ -35,7 +35,7 @@ describe('GET api/v1/search/tags', () => {
 describe('GET api/v1/search/topics', () => {
   it('Should return a list of related topics', (done) => {
     chai
-      .request(APP_URL)
+      .request(App)
       .get('/api/v1/search/topics?title=Deploying on Ubuntu')
       .end((err, response) => {
         if (err) return done(err);
@@ -46,7 +46,7 @@ describe('GET api/v1/search/topics', () => {
   });
   it('Should throw 400 if topic title is not provided', (done) => {
     chai
-      .request(APP_URL)
+      .request(App)
       .get('/api/v1/search/topics')
       .end((err, response) => {
         if (err) return done(err);
@@ -59,7 +59,7 @@ describe('GET api/v1/search/topics', () => {
 describe('GET api/v1/search/users/:id', () => {
   it('Should return a list of user participation activities', (done) => {
     chai
-      .request(APP_URL)
+      .request(App)
       .get('/api/v1/search/users/177960030022')
       .end((err, response) => {
         expect(response).to.have.status(200);
