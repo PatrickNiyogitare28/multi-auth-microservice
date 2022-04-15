8631ae8ba626343ca81496317bb5e653e13c9d13
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-04-15 03:41:58 +0200

    chore(cicd): added tests

--- test/unit/controllers/search-controller/github/search-controller.test.ts
+++ test/unit/controllers/search-controller/github/search-controller.test.ts
@@ -43,3 +43,42 @@ describe('GET api/v1/github/search/users', () => {
       });
   });
 });
+
+
+describe('GET api/v1/github/orgs/:organizationName', () => {
+  it('Should return 404 when organization not found', (done) => {
+    chai
+      .request(App)
+      .get('/api/v1/github/search/orgs/atlp-rwanda-n')
+      .end((err, response) => {
+        if (err) return done(err);
+        expect(response).to.have.status(404);
+        return done();
+      });
+  });
+  it('Should return 200 when organization is found', (done) => {
+    chai
+      .request(App)
+      .get('/api/v1/github/search/orgs/atlp-rwanda')
+      .end((err, response) => {
+        if (err) return done(err);
+        expect(response).to.have.status(200);
+        return done();
+      });
+  });
+});
+
+
+describe('GET api/v1/github/issues?q', () => {
+  it('Should return a list of issues', (done) => {
+    chai
+      .request(App)
+      .get('/api/v1/github/search/issues?q=Unable to compile typescript')
+      .end((err, response) => {
+        if (err) return done(err);
+        expect(response).to.have.status(200);
+        expect(response.body.data.items.length).greaterThanOrEqual(0);
+        return done();
+      });
+  });
+});
