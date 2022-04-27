fc04530e62ee53cdfd0c069f55b77755288924b3
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-04-27 08:14:40 +0200

    bug(access): added auth token

--- src/service/bitbucket-service/bitbucket.service.ts
+++ src/service/bitbucket-service/bitbucket.service.ts
@@ -3,10 +3,10 @@
 import axios from 'axios';
 const { get } = axios;
 export class BitBucketService {
-  public async searchByQuery(query: string, accessToken?: string): Promise<Record<string, unknown>> {
+  public async searchByQuery(query: string, authToken?: string): Promise<Record<string, unknown>> {
     let response;
     try {
-      const res = await get(query+`?access_token=${accessToken}`);
+      const res = await get(query+`?auth_token=${authToken}`);
       response = res?.data;
     } catch (e: any) {
       return e;
