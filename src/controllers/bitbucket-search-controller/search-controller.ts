1c4c7dbd4b21fb04d5295b3bc06332cb15107a7c
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-04-27 23:03:23 +0200

    ft(search-projects): added workplace projects

--- src/controllers/bitbucket-search-controller/search-controller.ts
+++ src/controllers/bitbucket-search-controller/search-controller.ts
@@ -6,20 +6,29 @@ import { CustomResponse } from '../../utils/custom-response';
 import { EStatusCode } from '../../enums/EStatusCode';
 import { BitBucketService } from '../../service/bitbucket-service/bitbucket.service';
 
-const { OK, BAD_REQUEST, NOT_FOUND } = EStatusCode;
+const { OK, BAD_REQUEST } = EStatusCode;
 export class BitBucketSearchController {
   private bitbucketService: BitBucketService;
+  private BITBUCKET_AUTH_TOKEN:string;
   private query: string;
   constructor(@Inject bitbucketService: BitBucketService) {
     this.bitbucketService = bitbucketService;
     this.query = `${process.env.BITBUCKET_API}`;
+    this.BITBUCKET_AUTH_TOKEN = process.env.BITBUCKET_AUTH_TOKEN as string;
   }
 
-  public async searchRepositories(req: Request, res: Response): Promise<Response> {
+  public async searchUserRepositories(req: Request, res: Response): Promise<Response> {
     const { username } = req.query;
     if (!username)
       return res.status(BAD_REQUEST).send(new CustomResponse(false, { message: 'user name is required' }));
-    const data = await this.bitbucketService.searchByQuery(this.query + `/repositories/${username}`, process.env.BITBUCKET_AUTH_TOKEN);
+    const data = await this.bitbucketService.searchByQuery(this.query + `/repositories/${username}`, this.BITBUCKET_AUTH_TOKEN);
+    return res.status(OK).send(new CustomResponse(true, data));
+  }
+
+  public async searchWorkplaceRepositories(req: Request, res: Response): Promise<Response>{
+    const {workplace} = req.params;
+    const data = await this.bitbucketService.searchByQuery(this.query+`/workspaces/${workplace}/projects`, this.BITBUCKET_AUTH_TOKEN);
     return res.status(OK).send(new CustomResponse(true, data));
   }
 }
+
