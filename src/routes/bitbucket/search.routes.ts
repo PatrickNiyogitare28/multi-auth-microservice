7444cdcec3c992d44a092a5b29d96f95f2b6deba
patrickniyogitare28 <patrickniyogitare28@gmail.com>
2022-04-29 22:09:26 +0200

    ft(bitbucket-search): added search codes

--- src/routes/bitbucket/search.routes.ts
+++ src/routes/bitbucket/search.routes.ts
@@ -9,11 +9,16 @@ router.get('/repositories', (req, res) => {
   searchController.searchUserRepositories(req, res);
 });
 
-router.get('/workplace/:workplace/projects', (req, res) => {
-  searchController.searchWorkplaceRepositories(req, res);
+router.get('/workspace/:workspace/projects', (req, res) => {
+  searchController.searchWorkSpaceRepositories(req, res);
 })
 
-router.get('/repositories/:workplace/:repoSlug/issues', (req, res) => {
-  searchController.searchWorkplaceRepositories(req, res);
+router.get('/repositories/:workspace/:repoSlug/issues', (req, res) => {
+  searchController.searchIssues(req, res);
 })
+
+router.get('/file-source/:workspace/:repository', (req, res) => {
+  searchController.searchFileSource(req, res);
+})
+
 export { router as BitBucketRouter }; 
